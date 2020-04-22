246d86fb27bd8b3302b0b02a8419c9e2cc61910b
FORGE-JITA\ktokt <ulfnew@gmail.com>
2020-04-22 19:21:37 +0500

    Remove unnecessary code

--- app-receiver.js
+++ app-receiver.js
@@ -2,13 +2,9 @@ firebase.initializeApp({
     messagingSenderId: messagingSettings.SenderID
 });
 
-
 var bt_register = $('#register');
 var bt_delete = $('#delete');
 var token = $('#token');
-var form = $('#notification');
-var massage_id = $('#massage_id');
-var massage_row = $('#massage_row');
 
 var info = $('#info');
 var info_message = $('#info-message');
@@ -16,118 +12,15 @@ var info_message = $('#info-message');
 var alert = $('#alert');
 var alert_message = $('#alert-message');
 
-var input_body = $('#body');
-var timerId = setInterval(setNotificationDemoBody, 10000);
-
-function setNotificationDemoBody() {
-    if (input_body.val().search(/^It's found today at \d\d:\d\d$/i) !== -1) {
-        var now = new Date();
-        input_body.val('It\'s found today at ' + now.getHours() + ':' + addZero(now.getMinutes()));
-    } else {
-        clearInterval(timerId);
-    }
-}
-
-function addZero(i) {
-    return i > 9 ? i : '0' + i;
-}
-
-setNotificationDemoBody();
 resetUI();
 
-if (
-    'Notification' in window &&
+const isEnable = 'Notification' in window &&
     'serviceWorker' in navigator &&
     'localStorage' in window &&
     'fetch' in window &&
-    'postMessage' in window
-) {
-    var messaging = firebase.messaging();
+    'postMessage' in window;
 
-    // already granted
-    if (Notification.permission === 'granted') {
-        getToken();
-    }
-
-    // get permission on subscribe only once
-    bt_register.on('click', function() {
-        getToken();
-    });
-
-    bt_delete.on('click', function() {
-        // Delete Instance ID token.
-        messaging.getToken()
-            .then(function(currentToken) {
-                messaging.deleteToken(currentToken)
-                    .then(function() {
-                        console.log('Token deleted');
-                        setTokenSentToServer(false);
-                        // Once token is deleted update UI.
-                        resetUI();
-                    })
-                    .catch(function(error) {
-                        showError('Unable to delete token', error);
-                    });
-            })
-            .catch(function(error) {
-                showError('Error retrieving Instance ID token', error);
-            });
-    });
-
-    form.on('submit', function(event) {
-        event.preventDefault();
-
-        var notification = {};
-        form.find('input').each(function () {
-            var input = $(this);
-            notification[input.attr('name')] = input.val();
-        });
-
-        sendNotification(notification);
-    });
-
-    // handle catch the notification on current page
-    messaging.onMessage(function(payload) {
-        console.log('Message received', payload);
-        info.show();
-        info_message
-            .text('')
-            .append('<strong>'+payload.data.title+'</strong>')
-            .append('<em>'+payload.data.body+'</em>')
-        ;
-
-        // register fake ServiceWorker for show notification on mobile devices
-        navigator.serviceWorker.register('/firebase-messaging-sw.js');
-        Notification.requestPermission(function (permission) {
-            if (permission === 'granted') {
-                navigator.serviceWorker.ready.then(function (registration) {
-                    // Copy data object to get parameters in the click handler
-                    payload.data.data = JSON.parse(JSON.stringify(payload.data));
-
-                    registration.showNotification(payload.data.title, payload.data);
-                }).catch(function (error) {
-                    // registration failed :(
-                    showError('ServiceWorker registration failed', error);
-                });
-            }
-        });
-    });
-
-    // Callback fired if Instance ID token is updated.
-    messaging.onTokenRefresh(function() {
-        messaging.getToken()
-            .then(function(refreshedToken) {
-                console.log('Token refreshed');
-                // Send Instance ID token to app server.
-                sendTokenToServer(refreshedToken);
-                updateUIForPushEnabled(refreshedToken);
-            })
-            .catch(function(error) {
-                showError('Unable to retrieve refreshed token', error);
-            });
-    });
-
-} else {
+if (!isEnable) {
     if (!('Notification' in window)) {
         showError('Notification not supported');
     } else if (!('serviceWorker' in navigator)) {
@@ -151,14 +44,88 @@ if (
     updateUIForPushPermissionRequired();
 }
 
+if (isEnable) {
+    var messaging = firebase.messaging();
+
+    // already granted
+    if (Notification.permission === 'granted') {
+        getToken();
+    }
+
+    // get permission on subscribe only once
+    bt_register.on('click', function () {
+        getToken();
+    });
+
+    bt_delete.on('click', function () {
+        // Delete Instance ID token.
+        messaging.getToken()
+            .then(function (currentToken) {
+                messaging.deleteToken(currentToken)
+                    .then(function () {
+                        console.log('Token deleted');
+                        setTokenSentToServer(false);
+                        // Once token is deleted update UI.
+                        resetUI();
+                    })
+                    .catch(function (error) {
+                        showError('Unable to delete token', error);
+                    });
+            })
+            .catch(function (error) {
+                showError('Error retrieving Instance ID token', error);
+            });
+    });
+
+    // handle catch the notification on current page
+    messaging.onMessage(function (payload) {
+        console.log('Message received', payload);
+        info.show();
+        info_message
+            .text('')
+            .append('<strong>' + payload.data.title + '</strong>')
+            .append('<em>' + payload.data.body + '</em>')
+        ;
+
+        // register fake ServiceWorker for show notification on mobile devices
+        navigator.serviceWorker.register('/firebase-messaging-sw.js');
+        Notification.requestPermission(function (permission) {
+            if (permission === 'granted') {
+                navigator.serviceWorker.ready.then(function (registration) {
+                    // Copy data object to get parameters in the click handler
+                    payload.data.data = JSON.parse(JSON.stringify(payload.data));
+
+                    registration.showNotification(payload.data.title, payload.data);
+                }).catch(function (error) {
+                    // registration failed :(
+                    showError('ServiceWorker registration failed', error);
+                });
+            }
+        });
+    });
+
+    // Callback fired if Instance ID token is updated.
+    messaging.onTokenRefresh(function () {
+        messaging.getToken()
+            .then(function (refreshedToken) {
+                console.log('Token refreshed');
+                // Send Instance ID token to app server.
+                sendTokenToServer(refreshedToken);
+                updateUIForPushEnabled(refreshedToken);
+            })
+            .catch(function (error) {
+                showError('Unable to retrieve refreshed token', error);
+            });
+    });
+}
 
 function getToken() {
     messaging.requestPermission()
-        .then(function() {
+        .then(function () {
             // Get Instance ID token. Initially this makes a network call, once retrieved
             // subsequent calls to getToken will return from cache.
             messaging.getToken()
-                .then(function(currentToken) {
+                .then(function (currentToken) {
 
                     if (currentToken) {
                         sendTokenToServer(currentToken);
@@ -169,59 +136,14 @@ function getToken() {
                         setTokenSentToServer(false);
                     }
                 })
-                .catch(function(error) {
+                .catch(function (error) {
                     showError('An error occurred while retrieving token', error);
                     updateUIForPushPermissionRequired();
                     setTokenSentToServer(false);
                 });
         })
-        .catch(function(error) {
+        .catch(function (error) {
             showError('Unable to get permission to notify', error);
-        });
-}
-
-
-function sendNotification(notification) {
-    var key = messagingSettings.ServerKey;
-
-    console.log('Send notification', notification);
-
-    // hide last notification data
-    info.hide();
-    massage_row.hide();
-
-    messaging.getToken()
-        .then(function(currentToken) {
-            fetch('https://fcm.googleapis.com/fcm/send', {
-                method: 'POST',
-                headers: {
-                    'Authorization': 'key=' + key,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    // Firebase loses 'image' from the notification.
-                    // And you must see this: https://github.com/firebase/quickstart-js/issues/71
-                    data: notification,
-                    to: currentToken
-                })
-            }).then(function(response) {
-                return response.json();
-            }).then(function(json) {
-                console.log('Response', json);
-
-                if (json.success === 1) {
-                    massage_row.show();
-                    massage_id.text(json.results[0].message_id);
-                } else {
-                    massage_row.hide();
-                    massage_id.text(json.results[0].error);
-                }
-            }).catch(function(error) {
-                showError(error);
-            });
-        })
-        .catch(function(error) {
-            showError('Error retrieving Instance ID token', error);
         });
 }
 
@@ -229,12 +151,14 @@ function sendNotification(notification) {
 // - send messages back to this app
 // - subscribe/unsubscribe the token from topics
 function sendTokenToServer(currentToken) {
-    if (!isTokenSentToServer(currentToken)) {
+    const wasSent = isTokenSentToServer(currentToken);
+    if (!wasSent) {
         console.log('Sending token to server...');
         // send current token to server
         //$.post(url, {token: currentToken});
         setTokenSentToServer(currentToken);
-    } else {
+    }
+    if (wasSent) {
         console.log('Token already sent to server so won\'t send it again unless it changes');
     }
 }
@@ -246,7 +170,8 @@ function isTokenSentToServer(currentToken) {
 function setTokenSentToServer(currentToken) {
     if (currentToken) {
         window.localStorage.setItem('sentFirebaseMessagingToken', currentToken);
-    } else {
+    }
+    if (!currentToken) {
         window.localStorage.removeItem('sentFirebaseMessagingToken');
     }
 }
@@ -256,15 +181,12 @@ function updateUIForPushEnabled(currentToken) {
     token.text(currentToken);
     bt_register.hide();
     bt_delete.show();
-    form.show();
 }
 
 function resetUI() {
     token.text('');
     bt_register.show();
     bt_delete.hide();
-    form.hide();
-    massage_row.hide();
     info.hide();
 }
 
@@ -274,7 +196,7 @@ function updateUIForPushPermissionRequired() {
 }
 
 function showError(error, error_data) {
-    if (typeof error_data !== "undefined") {
+    if (typeof error_data !== typeof undefined) {
         console.error(error, error_data);
     } else {
         console.error(error);
--- app-sender.js
+++ app-sender.js
@@ -1,133 +1,18 @@
-firebase.initializeApp({
-    messagingSenderId: messagingSettings.SenderID
-});
-
-
-var bt_register = $('#register');
-var bt_delete = $('#delete');
-var token = $('#token');
 var form = $('#notification');
 var massage_id = $('#massage_id');
 var massage_row = $('#massage_row');
-
-var info = $('#info');
-var info_message = $('#info-message');
+massage_row.hide();
 
 var alert = $('#alert');
 var alert_message = $('#alert-message');
 
-var input_body = $('#body');
-var timerId = setInterval(setNotificationDemoBody, 10000);
-
-function setNotificationDemoBody() {
-    if (input_body.val().search(/^It's found today at \d\d:\d\d$/i) !== -1) {
-        var now = new Date();
-        input_body.val('It\'s found today at ' + now.getHours() + ':' + addZero(now.getMinutes()));
-    } else {
-        clearInterval(timerId);
-    }
-}
-
-function addZero(i) {
-    return i > 9 ? i : '0' + i;
-}
-
-setNotificationDemoBody();
-resetUI();
-
-if (
-    'Notification' in window &&
+const isEnable = 'Notification' in window &&
     'serviceWorker' in navigator &&
     'localStorage' in window &&
     'fetch' in window &&
-    'postMessage' in window
-) {
-    var messaging = firebase.messaging();
+    'postMessage' in window;
 
-    // already granted
-    if (Notification.permission === 'granted') {
-        getToken();
-    }
-
-    // get permission on subscribe only once
-    bt_register.on('click', function () {
-        getToken();
-    });
-
-    bt_delete.on('click', function () {
-        // Delete Instance ID token.
-        messaging.getToken()
-            .then(function (currentToken) {
-                messaging.deleteToken(currentToken)
-                    .then(function () {
-                        console.log('Token deleted');
-                        setTokenSentToServer(false);
-                        // Once token is deleted update UI.
-                        resetUI();
-                    })
-                    .catch(function (error) {
-                        showError('Unable to delete token', error);
-                    });
-            })
-            .catch(function (error) {
-                showError('Error retrieving Instance ID token', error);
-            });
-    });
-
-    form.on('submit', function (event) {
-        event.preventDefault();
-
-        var notification = {};
-        form.find('input').each(function () {
-            var input = $(this);
-            notification[input.attr('name')] = input.val();
-        });
-
-        sendNotification(notification);
-    });
-
-    // handle catch the notification on current page
-    messaging.onMessage(function (payload) {
-        console.log('Message received', payload);
-        info.show();
-        info_message
-            .text('')
-            .append('<strong>' + payload.data.title + '</strong>')
-            .append('<em>' + payload.data.body + '</em>')
-        ;
-
-        // register fake ServiceWorker for show notification on mobile devices
-        navigator.serviceWorker.register('/firebase-messaging-sw.js');
-        Notification.requestPermission(function (permission) {
-            if (permission === 'granted') {
-                navigator.serviceWorker.ready.then(function (registration) {
-                    // Copy data object to get parameters in the click handler
-                    payload.data.data = JSON.parse(JSON.stringify(payload.data));
-
-                    registration.showNotification(payload.data.title, payload.data);
-                }).catch(function (error) {
-                    // registration failed :(
-                    showError('ServiceWorker registration failed', error);
-                });
-            }
-        });
-    });
-
-    // Callback fired if Instance ID token is updated.
-    messaging.onTokenRefresh(function () {
-        messaging.getToken()
-            .then(function (refreshedToken) {
-                console.log('Token refreshed');
-                // Send Instance ID token to app server.
-                sendTokenToServer(refreshedToken);
-                updateUIForPushEnabled(refreshedToken);
-            })
-            .catch(function (error) {
-                showError('Unable to retrieve refreshed token', error);
-            });
-    });
-
-} else {
+if (!isEnable) {
     if (!('Notification' in window)) {
         showError('Notification not supported');
     } else if (!('serviceWorker' in navigator)) {
@@ -147,134 +32,75 @@ if (
     console.log('Support LocalStorage', 'localStorage' in window);
     console.log('Support fetch', 'fetch' in window);
     console.log('Support postMessage', 'postMessage' in window);
-
-    updateUIForPushPermissionRequired();
 }
 
+if (isEnable) {
+    form.on('submit', function (event) {
+        event.preventDefault();
 
-function getToken() {
-    messaging.requestPermission()
-        .then(function () {
-            // Get Instance ID token. Initially this makes a network call, once retrieved
-            // subsequent calls to getToken will return from cache.
-            messaging.getToken()
-                .then(function (currentToken) {
-
-                    if (currentToken) {
-                        sendTokenToServer(currentToken);
-                        updateUIForPushEnabled(currentToken);
-                    } else {
-                        showError('No Instance ID token available. Request permission to generate one');
-                        updateUIForPushPermissionRequired();
-                        setTokenSentToServer(false);
-                    }
-                })
-                .catch(function (error) {
-                    showError('An error occurred while retrieving token', error);
-                    updateUIForPushPermissionRequired();
-                    setTokenSentToServer(false);
-                });
-        })
-        .catch(function (error) {
-            showError('Unable to get permission to notify', error);
+        var notification = {};
+        form.find('input').each(function () {
+            var input = $(this);
+            notification[input.attr('name')] = input.val();
         });
+
+        sendNotification(notification);
+    });
 }
 
+function getTokenFromServer() {
+    return window.localStorage.getItem('sentFirebaseMessagingToken');
+}
 
 function sendNotification(notification) {
     var key = messagingSettings.ServerKey;
 
-    console.log('Send notification', notification);
+    console.log('Sending notification', notification);
 
-    // hide last notification data
-    info.hide();
+    // hide previous notification data
+    alert.hide();
     massage_row.hide();
 
-    messaging.getToken()
-        .then(function (currentToken) {
-            fetch('https://fcm.googleapis.com/fcm/send', {
-                method: 'POST',
-                headers: {
-                    'Authorization': 'key=' + key,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    // Firebase loses 'image' from the notification.
-                    // And you must see this: https://github.com/firebase/quickstart-js/issues/71
-                    data: notification,
-                    to: currentToken
-                })
-            }).then(function (response) {
-                return response.json();
-            }).then(function (json) {
-                console.log('Response', json);
+    const currentToken = getTokenFromServer();
 
-                if (json.success === 1) {
-                    massage_row.show();
-                    massage_id.text(json.results[0].message_id);
-                } else {
-                    massage_row.hide();
-                    massage_id.text(json.results[0].error);
-                }
-            }).catch(function (error) {
-                showError(error);
-            });
-        })
-        .catch(function (error) {
-            showError('Error retrieving Instance ID token', error);
+    const success = typeof currentToken !== typeof undefined
+        && currentToken !== null;
+    if (!success) {
+        showError('Error retrieving Receiver Token');
+    }
+    if (success) {
+        fetch('https://fcm.googleapis.com/fcm/send', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'key=' + key,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                // Firebase loses 'image' from the notification.
+                // And you must see this: https://github.com/firebase/quickstart-js/issues/71
+                data: notification,
+                to: currentToken
+            })
+        }).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            console.log('Response', json);
+
+            if (json.success === 1) {
+                massage_row.show();
+                massage_id.text(json.results[0].message_id);
+            } else {
+                massage_row.hide();
+                massage_id.text(json.results[0].error);
+            }
+        }).catch(function (error) {
+            showError(error);
         });
-}
-
-// Send the Instance ID token your application server, so that it can:
-// - send messages back to this app
-// - subscribe/unsubscribe the token from topics
-function sendTokenToServer(currentToken) {
-    if (!isTokenSentToServer(currentToken)) {
-        console.log('Sending token to server...');
-        // send current token to server
-        //$.post(url, {token: currentToken});
-        setTokenSentToServer(currentToken);
-    } else {
-        console.log('Token already sent to server so won\'t send it again unless it changes');
     }
-}
-
-function isTokenSentToServer(currentToken) {
-    return window.localStorage.getItem('sentFirebaseMessagingToken') === currentToken;
-}
-
-function setTokenSentToServer(currentToken) {
-    if (currentToken) {
-        window.localStorage.setItem('sentFirebaseMessagingToken', currentToken);
-    } else {
-        window.localStorage.removeItem('sentFirebaseMessagingToken');
-    }
-}
-
-function updateUIForPushEnabled(currentToken) {
-    console.log(currentToken);
-    token.text(currentToken);
-    bt_register.hide();
-    bt_delete.show();
-    form.show();
-}
-
-function resetUI() {
-    token.text('');
-    bt_register.show();
-    bt_delete.hide();
-    form.hide();
-    massage_row.hide();
-    info.hide();
-}
-
-function updateUIForPushPermissionRequired() {
-    bt_register.attr('disabled', 'disabled');
-    resetUI();
 }
 
 function showError(error, error_data) {
-    if (typeof error_data !== "undefined") {
+    if (typeof error_data !== typeof undefined) {
         console.error(error, error_data);
     } else {
         console.error(error);
